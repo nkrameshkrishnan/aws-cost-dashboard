ac60156a22b99bb0b04890a60677dca6f6638085
rameshkrishnan-narashimankrishnamurthy <ramesh_krishnan@trendmicro.com>
2026-02-28 07:24:34 -0500

    Switch to HashRouter for GitHub Enterprise Pages compatibility

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './styles/index.css'
@@ -19,9 +19,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/pages/rameshkrishnan-narashimankrishnamurthy/aws-cost-dashboard">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
